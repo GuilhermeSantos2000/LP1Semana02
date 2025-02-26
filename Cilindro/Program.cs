34cce37ab1af2ed6088f1bd91bda319e6e095173
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-02-26 19:47:46 +0000

    Pedir valores do cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere o valor da altura do cilindro:");            
+            string altura = Console.ReadLine();
+            double a = double.Parse(altura);
+
+            Console.WriteLine("Insere o valor do raio do cilindro:");            
+            string raio = Console.ReadLine();
+            double r = double.Parse(raio);
+        }
+    }
+}
