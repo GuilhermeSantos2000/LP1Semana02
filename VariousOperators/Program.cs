285ad8516dda4e2cc55d0044f39b76f2ca1c6c25
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-02-26 19:37:28 +0000

    Operadores

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere número inteiro-não negativo:");            
+            string str = Console.ReadLine();
+            byte a = byte.Parse(str);
+
+            byte b = (byte)(a / 2);
+            byte c = (byte)(a << 3);  
+            byte d = (byte)(a ^ 6);
+            bool e = a > 10;          
+
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+        }
+    }
+}
