868e6980bd642bb4f4b0c5b57826a34741306da3
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-02-26 20:20:50 +0000

    carateres infinitos e NaN

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -31,6 +31,13 @@ namespace ValoresEspeciais
             decimal MaxDecimal = decimal.MaxValue;
             decimal MinDecimal = decimal.MinValue;
 
+            float PositiveInfiniteFloat = float.PositiveInfinity;
+            float NegativeInfiniteFloat = float.NegativeInfinity;
+            float NotANumberFloat = float.NaN;
+            double PositiveInfiniteDouble = double.PositiveInfinity;
+            double NegativeInfiniteDouble = double.NegativeInfinity;
+            double NotANumberDouble = double.NaN;
+
             Console.WriteLine(MaxByte);
             Console.WriteLine(MinByte);
             Console.WriteLine(MaxShort);
@@ -55,6 +62,13 @@ namespace ValoresEspeciais
             Console.WriteLine(MinDouble);
             Console.WriteLine(MaxDecimal);
             Console.WriteLine(MinDecimal);
+
+            Console.WriteLine(PositiveInfiniteFloat);
+            Console.WriteLine(NegativeInfiniteFloat);
+            Console.WriteLine(NotANumberFloat);
+            Console.WriteLine(PositiveInfiniteDouble);
+            Console.WriteLine(NegativeInfiniteDouble);
+            Console.WriteLine(NotANumberDouble);
         }
     }
 }
